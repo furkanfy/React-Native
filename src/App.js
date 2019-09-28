3e76c1023ba43a268efb9b1bd5adfcb570b109f7
Your Name <you@example.com>
2019-09-28 20:45:38 +0300

    ilk component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import User from "./components/user.js"
 import './App.css';
 
-function App() {
+
+class App extends Component {
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>
+        App component
+      </h4>
+      <User/>
+     
     </div>
   );
+  }
 }
 
 export default App;
---
+++ src/components/user.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class user extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text"/>
+                    <button>gönder</button></form>                
+            </div>
+        )
+    }
+}
